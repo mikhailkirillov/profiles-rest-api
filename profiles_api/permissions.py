23cc4eaca738e15cdfed86ef997b64de65178483
mikhailkirillov <86004125+mikhailkirillov@users.noreply.github.com>
2021-12-21 14:03:50 +0100

    Added login API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class UpdateOwnProfile(permissions.BasePermission):
-    """Allow user to edit their own profile"""
+    """Allow users to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
